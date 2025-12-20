b9218780468c2d773d84864bf2feccb90e6368d9
Jacques Thurling <jthurling78@gmail.com>
2025-12-20 13:57:29 +0200

    Added the variabled

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ let config = {
   processing: true
 }
 
-let numbers = [];
+let stack = [];
+
+let availableVariableNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let variables = {}; 
+
 let operations = {
   '+': (a, b) => a + b,
   '-': (a, b) => a - b,
@@ -12,58 +16,121 @@ let operations = {
   '*': (a, b) => a * b
 }
 
+function checkIfVariable(character) {
+  for (let index = 0; index < availableVariableNames.length; index++) {
+    const char = availableVariableNames[index];
+    
+    if(char === character) return character;
+  }
+}
+
 function parseInput(input) {
   let chars = input.split(" ");
 
-  for (const char of chars) {
-    switch (char) {
+  for (let i = 0; i < chars.length; i++) {
+    switch (chars[i]) {
+      case '=':
+        if (stack.length > 1) {
+          let a = stack.pop(), b = stack.pop();
+
+          variables[b] = a;
+        }
+        break;
       case '+':
-        if (numbers.length > 1) {
-          let a = numbers.pop(), b = numbers.pop();
-          let result = operations[char](a, b);
-          numbers.push(result);
+        if (stack.length > 1) {
+          let a = stack.pop(), b = stack.pop();
+
+          let num1 = b, num2 = a;
+
+          if (b in variables) {
+            num1 = variables[b];
+          }
+
+          if (a in variables) {
+            num2 = variables[a];
+          }
+
+          let result = operations[chars[i]](num1, num2);
+          stack.push(result);
         } else {
-          return numbers.pop();
+          return stack.pop();
         }
         break;
       case '-':
-        if (numbers.length > 1) {
-          let a = numbers.pop(), b = numbers.pop();
-          let result = operations[char](a, b);
-          numbers.push(result);
+        if (stack.length > 1) {
+          let a = stack.pop(), b = stack.pop();
+
+          let num1 = b, num2 = a;
+
+          if (b in variables) {
+            num1 = variables[b];
+          }
+
+          if (a in variables) {
+            num2 = variables[a];
+          }
+
+          let result = operations[chars[i]](num1, num2);
+          stack.push(result);
         } else {
-          return numbers.pop();
+          return stack.pop();
         }
         break;
       case '/':
-        if (numbers.length > 1) {
-          let a = numbers.pop(), b = numbers.pop();
-          let result = operations[char](a, b);
-          numbers.push(result);
+        if (stack.length > 1) {
+          let a = stack.pop(), b = stack.pop();
+
+          let num1 = b, num2 = a;
+
+          if (b in variables) {
+            num1 = variables[b];
+          }
+
+          if (a in variables) {
+            num2 = variables[a];
+          }
+
+          let result = operations[chars[i]](num1, num2);
+          stack.push(result);
         } else {
-          return numbers.pop();
+          return stack.pop();
         }
         break;
       case '*':
-        if (numbers.length > 1) {
-          let a = numbers.pop(), b = numbers.pop();
-          let result = operations[char](a, b);
-          numbers.push(result);
+        if (stack.length > 1) {
+          let a = stack.pop(), b = stack.pop();
+
+          let num1 = b, num2 = a;
+
+          if (b in variables) {
+            num1 = variables[b];
+          }
+
+          if (a in variables) {
+            num2 = variables[a];
+          }
+
+          let result = operations[chars[i]](num1, num2);
+          stack.push(result);
         } else {
-          return numbers.pop();
+          return stack.pop();
         }
         break;
       default:
-        const num = Number(char);
+        const num = Number(chars[i]);
 
         if (Number.isFinite(num)) {
-          numbers.push(num);
+          stack.push(num);
+        }
+
+        if (checkIfVariable(chars[i])) {
+          stack.push(chars[i]);
         }
         break;
     }
   }
 
-  return numbers.pop();
+  return stack.pop();
 }
 
 function ask(question) {
@@ -90,7 +157,9 @@ function ask(question) {
     }
 
     let output = parseInput(input);
-
     console.log(output);
+
+    // Clear the stack
+    stack = [];
   }
 })();
